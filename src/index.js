1b0712aca2a947af3c8549fd4fc772da6d1d809c
duskstone <jedgardromero@gmail.com>
2020-05-15 10:33:38 -0400

    Initial Commit, Wepack Setup

---
+++ src/index.js
@@ -0,0 +1,9 @@
+window.$1 = function(arg) {
+    // debugger
+    // undercase document not uppercase/ also this.document to link current doc
+    // ^ results to querySelectorAll not function
+    this.document.querySelectorAll(arg);
+    // console.log("log");
+};
+
+// module.exports = index.js 
