206a00d880d68712260e4a841c4b775d7ea810e7
duskstone <jedgardromero@gmail.com>
2020-05-15 12:04:33 -0400

    added RemoveClass, removing className value

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 const DOMNodeCollection = require("./dom_node_collection");
 
 window.$1 = function(arg) {
-    // debugger
+    if (arg instanceof HTMLElement ) {
+        return new DOMNodeCollection([arg]);
+    } else { 
     // undercase document not uppercase/ also this.document to link current doc
     // ^ results to querySelectorAll not function
-    const nodeList = this.document.querySelectorAll(arg);
-    // console.log("log");
-    return this.Array.from(nodeList);
+        const nodeList = this.document.querySelectorAll(arg);
+        // console.log("log");
+        const nodes = this.Array.from(nodeList);
+        return new DOMNodeCollection(nodes);
+    };
 };
 
 // module.exports = index.js 
